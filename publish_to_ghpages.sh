5563a5a8533d145e15c429e7d47d05e0b33a3dff
cheng.zhang <mr.cheng94@foxmail.com>
2019-10-23 15:01:37 +0800

    修改upstream路径

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -14,7 +14,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git worktree add -B gh-pages public origin	https://github.com/Sailfishc/sailfishc.github.io.git/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
