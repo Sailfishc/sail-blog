5b39956d5c934d2ffabb60a2d6cf018b857d0457
cheng.zhang <mr.cheng94@foxmail.com>
2019-10-23 15:12:41 +0800

    修改remote源

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -14,7 +14,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin git@github.com:sailfishc/sailfishc.github.io.git/gh-pages
+git worktree add -B gh-pages public git@github.com:sailfishc/sailfishc.github.io.git/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
