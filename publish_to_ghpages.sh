d692db0b1bb181fb0ac1d383e8b33c370b171d47
cheng.zhang <mr.cheng94@foxmail.com>
2019-10-23 15:17:14 +0800

    修改为原来的origin

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -14,7 +14,9 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public git@github.com:sailfishc/sailfishc.github.io.git/gh-pages
+#git worktree add -B gh-pages public git@github.com:sailfishc/sailfishc.github.io.git/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
+
 
 echo "Removing existing files"
 rm -rf public/*
