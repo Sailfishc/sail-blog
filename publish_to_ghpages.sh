596d6ebf1c25acfedde5e57ac558a297ba885184
cheng.zhang <mr.cheng94@foxmail.com>
2019-10-23 15:23:06 +0800

    修改push逻辑

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -28,4 +28,4 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 #echo "Pushing to github"
-git push --all origin git@github.com:sailfishc/sailfishc.github.io.git/gh-pages
+git push --all origin git@github.com:sailfishc/sailfishc.github.io.git gh-pages
